b7c9cc5ee6e36470dd39c09f887a1067a5d65eae
Denwiliusz27 <danwielgosz@gmail.com>
2020-10-24 15:24:39 +0200

    zad1 v0.3

--- zad1/main.py
+++ zad1/main.py
@@ -1,12 +1,25 @@
+def is_number(input_string):
+    liczba = input_string
+
+    while not (liczba.isnumeric()):
+        print()
+        liczba = input("Niepoprawne dane, wpisz ponownie: ")
+
+    return liczba
+
+
 def main():
-    ilosc = input("Podaj liczbe nieparzyta: ")
+    ilosc = input("Podaj liczbe nieparzyta dodatnia: ")
     space = 0
 
+    ilosc = int(is_number(ilosc))
 
-    while (ilosc % 2 == 0) or (ilosc < 0):
-        print("Podano niepoprawna liczbe.")
-        ilosc = int(input("Podaj ponownie: "))
+    while ilosc % 2 == 0:
+        print()
+        ilosc = input("Podana liczba nie jest nieparzysta, podaj ponownie: ")
+        ilosc = int(is_number(ilosc))
 
+    print("\nWpisano liczbe: " + str(ilosc) + "\n")
     while ilosc > 0:
         for j in range(space):
             print(' ', end=""),
@@ -15,5 +28,6 @@ def main():
         ilosc -= 2
         space += 1
         print()
+
 
 main()
