074736e5b912bdb1441748309b94b36c2200ef99
Denwiliusz27 <danwielgosz@gmail.com>
2020-10-31 18:36:39 +0100

    zad_2 v0.2

--- Zestaw_2/zadanie_2/zad_2.py
+++ Zestaw_2/zadanie_2/zad_2.py
@@ -1,1 +1,29 @@
-print("hello")
+def is_positive_number(input_string):
+    liczba = input_string
+
+    while not (liczba.isnumeric()):
+        print()
+        liczba = input("Niepoprawne dane, wpisz ponownie: ")
+
+    return liczba
+
+
+def is_in_range(liczba):
+    while 1:
+        if liczba < 1900 or liczba > 100000:
+            liczba = input("\nLiczba nie nalezy do zakresu, wpisz ponownie: ")
+            liczba = is_positive_number(liczba)
+            break
+
+    return liczba
+
+
+def main():
+    rok = input("Podaj rok z zakresu 1900 - 100000: ")
+    rok = is_positive_number(rok)
+    rok = is_in_range(rok)
+    print("Wpisano: ", rok)
+
+
+if __name__ == '__main__':
+    main()
