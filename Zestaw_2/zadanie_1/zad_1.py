0b6fcf7f67d99e8c7a99f1594a220a7dc2811a7e
Denwiliusz27 <danwielgosz@gmail.com>
2020-10-31 18:00:33 +0100

    zad_1 v0.5

--- Zestaw_2/zadanie_1/zad_1.py
+++ Zestaw_2/zadanie_1/zad_1.py
@@ -14,9 +14,21 @@ def add(lista, element):
     return
 
 
+def longest(lista):
+    count = 0
+
+    for i in lista:
+        if type(i) == list:
+            count += 1
+            longest(i)
+
+    return count
+
+
 def longest_nested_list(lista):
     length = 0
     temp = 0
+    index = 0
     #print("wchodze z : ", lista)
     print("indeks = ", temp)
 
@@ -24,17 +36,21 @@ def longest_nested_list(lista):
         if isinstance(i, list):
             length += 1
             print("mam liste: ", i)
+            index = lista.index(i)
             temp += longest_nested_list(i)
 
         if temp > length:
             length = temp
+
     return length
 
 
 
 
 def main():
-    lista = [1, 2, [3, 4, [5, 6], 5], 3, [2, [3, [4, 6]]], 4]
+    lista = [1, 2, [3, 4, [5, 6], 5], 3, 4]
+    lista1 = [1, [2, 3], 4]
+    lista2 = [3, 4, [2, [1, 2, [7, 8], 3, 4], 3, 4], 5, 6, 7]
     amount_of_nested_list = 0
     index = 0
     indeksy = []
@@ -42,7 +58,13 @@ def main():
     add(lista, 99)
     print(lista)
 
-    print(longest_nested_list(lista))
+    add(lista1, 99)
+    print(lista1)
+
+    add(lista2, 99)
+    print(lista2)
+
+   # print(longest_nested_list(lista))
 
 """    
     for i in lista:
