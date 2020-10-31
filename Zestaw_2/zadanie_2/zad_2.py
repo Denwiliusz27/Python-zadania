4693b604e671b4a6044f81a8bc660c130a3ddb88
Denwiliusz27 <danwielgosz@gmail.com>
2020-10-31 20:41:41 +0100

    zad_2 v0.4

--- Zestaw_2/zadanie_2/zad_2.py
+++ Zestaw_2/zadanie_2/zad_2.py
@@ -18,7 +18,19 @@ def is_positive_number(input_string):
 def main():
     rok = input("Podaj rok z zakresu 1900 - 100000: ")
     rok = is_positive_number(rok)
-    print("Wpisano: ", rok)
+
+    print()
+
+    if rok % 4 != 0:
+        print(False)
+    else:
+        if rok % 100 == 0:
+            if rok % 400 == 0:
+                print(True)
+            else:
+                print(False)
+        else:
+            print(True)
 
 
 if __name__ == '__main__':
