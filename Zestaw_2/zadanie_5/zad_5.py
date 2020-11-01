b69918e91acd8ef319ce884a758a548d65a01879
Denwiliusz27 <danwielgosz@gmail.com>
2020-11-01 13:38:14 +0100

    zad_5 v0.2

--- Zestaw_2/zadanie_5/zad_5.py
+++ Zestaw_2/zadanie_5/zad_5.py
@@ -1,7 +1,7 @@
 def is_positive_number(input_string):
     liczba = input_string
 
-    while not (liczba.isnumeric()) or int(liczba) == 0:
+    while not (liczba.isnumeric()) or int(liczba) == 0 or int(liczba) > 2147483647:
         print()
         liczba = input("Niepoprawne dane, wpisz ponownie: ")
 
@@ -10,11 +10,17 @@ def is_positive_number(input_string):
 
 def fun(N):
     liczba = ""
+    dl_przerwy = 0
 
-    for i in bin(N)[2:]:
-        liczba += i
+    while N != 0:
+        if N % 2 == 0:
+            liczba = "0" + liczba
+        else:
+            liczba = "1" + liczba
+        N = N // 2
 
-    
+    for znak in liczba:
+
 
     return liczba
 
@@ -23,8 +29,7 @@ def main():
     liczba = input("Podaj liczbe w systemie dziesietnym z przedzialu [1, 2147483647]: ")
     liczba = int(is_positive_number(liczba))
 
-    liczba1 = fun(liczba)
-    print(liczba1)
+    print(liczba)
 
 
 if __name__ == '__main__':
