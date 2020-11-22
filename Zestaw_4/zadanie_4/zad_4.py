9295e63d59b9e6c06b8dfe648c538e260a0c4f85
Denwiliusz27 <danwielgosz@gmail.com>
2020-11-22 12:49:56 +0100

    v2.0 - zad4 - two dictionaries

--- Zestaw_4/zadanie_4/zad_4.py
+++ Zestaw_4/zadanie_4/zad_4.py
@@ -8,18 +8,29 @@ letters = {
     "M": 1000
 }
 
+# słownik alternatywny
+letters_v2 = dict([
+    ("I", 1),
+    ("V", 5),
+    ("X", 10),
+    ("L", 50),
+    ("C", 100),
+    ("D", 500),
+    ("M", 1000)
+])
+
 
 def roman2int(liczba):
     suma = 0
-    pos = len(liczba)-1
+    pos = len(liczba) - 1
 
     while pos >= 0:
         letter = liczba[pos]
 
-        if pos < len(liczba)-1:
-            if letters[letter] < letters[liczba[pos+1]]:
+        if pos < len(liczba) - 1:
+            if letters[letter] < letters[liczba[pos + 1]]:
                 suma -= letters[letter]
-            elif letters[letter] >= letters[liczba[pos+1]]:
+            elif letters[letter] >= letters[liczba[pos + 1]]:
                 suma += letters[letter]
         else:
             suma += letters[letter]
@@ -30,4 +41,4 @@ def roman2int(liczba):
 
 if __name__ == '__main__':
     liczba = "MMCCCLXXVII"
-    print(liczba," =", roman2int(liczba))
+    print(liczba, " =", roman2int(liczba))
