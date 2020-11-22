50e9cf909b3a896cb53d85276eff3d6b8e287a29
Denwiliusz27 <danwielgosz@gmail.com>
2020-11-22 18:21:36 +0100

    v1.0 - zad2 - working

--- Zestaw_5/zadanie_2/zad_2.py
+++ Zestaw_5/zadanie_2/zad_2.py
@@ -2,12 +2,17 @@ import functools
 
 
 def pamiec(func):
-    dictionary = {0: 0, 1: 1}
+    dictionary = {}
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
+        print("wrap(", args[0], ")")
         if args[0] not in dictionary:
-            dictionary[args[0]] = wrapper(args[0] - 1) + wrapper(args[0] - 2)
+            print("dodaje do slownika ")
+            # dictionary[args[0]] = wrapper(args[0] - 1) + wrapper(args[0] - 2)
+            dictionary[args[0]] = func(args[0])
+        print(dictionary)
+        print("~~~~~", args[0], "~~~~~~~")
 
         return dictionary[args[0]]
 
@@ -21,10 +26,15 @@ def pamiec(func):
 
 @pamiec
 def fibonacci(n):
+    print("fib(", n, ")")
     return n if 0 <= n < 2 else fibonacci(n - 1) + fibonacci(n - 2)
 
 
 if __name__ == '__main__':
     for i in range(10):
         print(fibonacci(i))
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+    print("###########")
+    print(fibonacci(5))
 
