a40d374337411c1b2f8d34ddf8db45002e352d87
Denwiliusz27 <danwielgosz@gmail.com>
2020-11-29 16:17:01 +0100

    v1.0 - first working version

--- Zestaw_6/zadanie_1/zad_1.py
+++ Zestaw_6/zadanie_1/zad_1.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Point:
     """Klasa reprezentująca punkty na płaszczyźnie."""
 
@@ -6,12 +8,12 @@ class Point:
         self.y = y
 
     def __str__(self):  # zwraca string "(x, y)"
-        return "({0}, {1})".format(self.x, self.y)
+        return "({}, {})".format(self.x, self.y)
 
     def __repr__(self):  # zwraca string "Point(x, y)"
-        return "Point({0}, {1})".format(self.x, self.y)
+        return "Point({}, {})".format(self.x, self.y)
 
-    def __eq__(self, other):   # obsługa point1 == point2
+    def __eq__(self, other):  # obsługa point1 == point2
         return (self.x == other.x) and (self.y == other.y)
 
     def __ne__(self, other):  # obsługa point1 != point2
@@ -21,19 +23,20 @@ class Point:
     def __add__(self, other):  # v1 + v2
         return Point(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other):   # v1 - v2
+    def __sub__(self, other):  # v1 - v2
         return Point(self.x - other.x, self.y - other.y)
-       # self.x -= other.x
-       # self.y -= other.y
 
-    def __mul__(self, other):   # v1 * v2, iloczyn skalarny (liczba)
+    # self.x -= other.x
+    # self.y -= other.y
+
+    def __mul__(self, other):  # v1 * v2, iloczyn skalarny (liczba)
         return self.x * other.x + self.y * other.y
 
     def cross(self, other):  # v1 x v2, iloczyn wektorowy 2D (liczba)
         return self.x * other.y - self.y * other.x
 
-    def length(self):   # długość wektora
-        return self.x**2 + self.y**2
+    def length(self):  # długość wektora
+        return self.x ** 2 + self.y ** 2
 
     def __hash__(self):
         return hash((self.x, self.y))  # bazujemy na tuple, immutable points
@@ -41,11 +44,45 @@ class Point:
 
 # Kod testujący moduł.
 
-# import unittest
+class TestPoint(unittest.TestCase):
+    def setUp(self): pass
 
-# class TestPoint(unittest.TestCase): pass
+    def test_print(self): pass
+
+    def test_cmp(self):
+        self.assertTrue(Point(2, 3) == Point(2, 3))
+        self.assertFalse(Point(2, 3) == Point(3, 2))
+        self.assertTrue(Point(2, 3) != Point(3, 2))
+        self.assertFalse(Point(2, 3) != Point(2, 3))
+
+    def test_add(self):
+        self.assertEqual(Point(2, 3) + Point(3, 2), Point(5, 5))
+        self.assertNotEqual(Point(2, 3) + Point(3, 2), Point(-1, 1))
+
+    def test_sub(self):
+        self.assertEqual(Point(2, 3) - Point(3, 2), Point(-1, 1))
+        self.assertNotEqual(Point(2, 3) - Point(3, 2), Point(5, 5))
+
+    def test_mul(self):
+        self.assertEqual(Point(2, 3) * Point(3, 2), 12)
+        self.assertNotEqual(Point(2, 3) * Point(3, 2), 5)
+
+    def test_cross(self):
+        self.assertEqual(Point(2, 3).cross(Point(3, 2)), -5)
+        self.assertNotEqual(Point(2, 3).cross(Point(3, 2)), 0)
+
+    def test_length(self):
+        self.assertEqual(Point(2, 3).length(), 13)
+        self.assertNotEqual(Point(2, 3).length(), 5)
+
 
 if __name__ == '__main__':
+    unittest.main()
+"""
+    p1 = Point(1, 2)
+    p2 = Point(3, 4)
+    print(p1.cross(p2))
+
     p1 = Point(4, 5)
     p2 = Point(3, 2)
     print(p1 != p2)
@@ -61,8 +98,9 @@ if __name__ == '__main__':
     print("p1 - p2: ", p1.__sub__(p2))
     print("p1 * p2: ", p1.__mul__(p2))
     print("p1 x p2: ", p1.cross(p2))
-    print("|p1|", p1.length())
+    print("|p1|: ", p1.length())
     print("#################")
 
     print(p1.length())
     print(p1)
+"""
