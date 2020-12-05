f1e593b5ffde66c04e3d6e18733f34728a6b5add
Denwiliusz27 <danwielgosz@gmail.com>
2020-12-05 19:45:42 +0100

    v0.2 - all methods

--- Zestaw_7/zadanie_1/zad_1.py
+++ Zestaw_7/zadanie_1/zad_1.py
@@ -12,19 +12,19 @@ class Rectangle:
         else:
             raise ValueError
 
-    def __str__(self): # "[(x1, y1), (x2, y2)]"
-        return "[{0}, {1}]" .format(self.pt1, self.pt2)
+    def __str__(self):  # "[(x1, y1), (x2, y2)]"
+        return "[{0}, {1}]".format(self.pt1, self.pt2)
 
     def __repr__(self):  # "Rectangle(x1, y1, x2, y2)"
         return "Rectanngle({0}, {1}, {2}, {3})".format(self.pt1.x, self.pt1.y, self.pt2.x, self.pt2.y)
 
-    def __eq__(self, other): # obsługa rect1 == rect2
+    def __eq__(self, other):  # obsługa rect1 == rect2
         return self.pt1 == other.pt1 and self.pt2 == other.pt2
 
     def __ne__(self, other):  # obsługa rect1 != rect2
         return not self == other
 
-    def center(self): # zwraca środek prostokąta
+    def center(self):  # zwraca środek prostokąta
         x = (self.pt2.x + self.pt1.x) / 2
         y = (self.pt2.y + self.pt1.y) / 2
         return Point(x, y)
@@ -32,44 +32,38 @@ class Rectangle:
     def area(self):  # pole powierzchni
         x = self.pt2.x - self.pt1.x
         y = self.pt2.y - self.pt1.y
-        return x*y
+        return x * y
 
     def move(self, x, y):  # przesunięcie o (x, y)
         self.pt1 += Point(x, y)
         self.pt2 += Point(x, y)
         return self
 
-
     def intersection(self, other):  # część wspólna prostokątów
         x = min(self.pt2.x, other.pt2.x) - max(self.pt1.x, other.pt1.x)
         y = min(self.pt2.y, other.pt2.y) - max(self.pt1.y, other.pt1.y)
-        if x >= 0 and y >=0:
-            return x*y
+
+        if x >= 0 and y >= 0:
+            return x * y
         else:
             return 0
 
-        """if self.pt1.x <= other.pt1.x <= self.pt2.x:
-            print("warunek1")
-            if self.pt1.y <= other.pt1.y <= self.pt2.y:
-                print("warunek2")
-                if self.pt1.x <= other.pt2.x <= self.pt2.x:
-                    print("warunek3")
-                    if self.pt1.y <= other.pt2.y <= self.pt2.y:
-                        print("warunek4")
-                        return other.area()  # jeden wewnatrz drugiego
-                    else:  # jeden wychodzi z gory drugiego
-                        print("warunek5")
-                        p1 = other.pt1
-                        p2 = Point(other.pt2.x, self.pt2.y)
-                        return Rectangle(p1.x, p1.y, p2.x, p2.y).area()
-        """
+    def cover(self, other):  # prostąkąt nakrywający oba
+        x1 = min(self.pt1.x, other.pt1.x)
+        y1 = min(self.pt1.y, other.pt1.y)
+        x2 = max(self.pt2.x, other.pt2.x)
+        y2 = max(self.pt2.y, other.pt2.y)
+        return Rectangle(x1, y1, x2, y2)
 
-    def cover(self, other):
-        pass  # prostąkąt nakrywający oba
+    def make4(self):  # zwraca krotkę czterech mniejszych
+        center = self.center()
 
-    def make4(self):
-        pass  # zwraca krotkę czterech mniejszych
+        r1 = Rectangle(self.pt1.x, self.pt1.y, center.x, center.y)
+        r2 = Rectangle(center.x, center.y, self.pt2.x, self.pt2.y)
+        r3 = Rectangle(self.pt1.x, center.y, center.x, self.pt2.y)
+        r4 = Rectangle(center.x, self.pt1.y, self.pt2.x, center.y)
 
+        return r1, r2, r3, r4
 
 # Kod testujący moduł.
 
@@ -92,7 +86,6 @@ if __name__ == '__main__':
     finally:
         print("zrobilem")
 
-
     print(rec1)
     print(rec2)
     print(rec1.__repr__())
@@ -102,11 +95,13 @@ if __name__ == '__main__':
     print(rec1.center())
     print(rec1.area())
     print("#####cz_wspolna######")
-    rec3 = Rectangle(4, 1, 6, 5)
+    rec3 = Rectangle(1, 1, 3, 3)
     print(rec1.intersection(rec3))
     print("#####cz_wspolna######")
-
+    print(rec1.cover(rec3))
     print(rec1.move(3, -2))
+    print(rec1.make4())
+
 
 """  try:
         rec2 = Rectangle(3, 3, 1, 1)
