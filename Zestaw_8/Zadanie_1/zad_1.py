8c63c707774a7d27c7b6c00c888e53c59c33aa49
Denwiliusz27 <danwielgosz@gmail.com>
2020-12-30 12:02:32 +0100

    v0.02 - deal with json

--- Zestaw_8/Zadanie_1/zad_1.py
+++ Zestaw_8/Zadanie_1/zad_1.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import json
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    with open('przystanki.json', "r", encoding='utf-8') as read_file:
+        przystanki = json.load(read_file)
+
+    with open('tramwaje.json', "r", encoding='utf-8') as read_file:
+        tramwaje = json.load(read_file)
+
+    ilosc_linii = len(tramwaje["tramwaje"])
+
+    #for i in tramwaje["tramwaje"][0]["tprzystanki"]:
+     #   print(i["name"])
 
 
-# Press the green button in the gutter to run the script.
+
+    graph = dict()
+    for i in przystanki["przystanki"]:
+        graph[i] = list()
+
+
+
+    for i in range(1):
+        tprzystanki = tramwaje["tramwaje"][i]["tprzystanki"]
+        lista_przystankow = list()
+
+        for j in tprzystanki:
+            lista_przystankow.append(j["name"])
+
+        print(lista_przystankow)
+
+
+     #  for j in range(ilosc_linii):
+        #    tprzystanki = tramwaje["tramwaje"][j]["tprzystanki"]
+         #   for n in tprzystanki:
+            #    if n["name"] == ""
+
+         #   for n in tprzystanki:
+           #     print(n)
+
+    print(tramwaje["tramwaje"][0]["name"])
+
+   # graph["Agatowa"].append(("plac", 1))
+    #graph["Agatowa"].append(("rondo", 1))
+
+   # print(graph["Agatowa"])
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
