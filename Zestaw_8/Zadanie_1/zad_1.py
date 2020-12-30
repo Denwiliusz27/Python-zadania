ae18765752d0e60bde8b07b40d00c234a80f8102
Denwiliusz27 <danwielgosz@gmail.com>
2020-12-30 12:47:26 +0100

    v0.11 - clear code#1

--- Zestaw_8/Zadanie_1/zad_1.py
+++ Zestaw_8/Zadanie_1/zad_1.py
@@ -1,27 +1,12 @@
 import json
 
 
-def create_graph():
-    with open('przystanki.json', "r", encoding='utf-8') as read_file:
-        przystanki = json.load(read_file)
-
-    with open('tramwaje.json', "r", encoding='utf-8') as read_file:
-        tramwaje = json.load(read_file)
-
+def create_graph(tramwaje):
     ilosc_linii = len(tramwaje["tramwaje"])
 
-    #for i in tramwaje["tramwaje"][0]["tprzystanki"]:
-     #   print(i["name"])
-
-
-
     graph = dict()
-    #for i in przystanki["przystanki"]:
-        #graph[i] = list()
 
-
-
-    for i in range(ilosc_linii):  # ilosc_linii
+    for i in range(ilosc_linii):
         tprzystanki = tramwaje["tramwaje"][i]["tprzystanki"]
         lista_przystankow = list()
 
@@ -30,43 +15,33 @@ def create_graph():
                 graph[j["name"]] = list()
             lista_przystankow.append(j["name"])
 
-
         for n in range(len(lista_przystankow)):
             if n == 0:
                 if not (lista_przystankow[1], 1) in graph[lista_przystankow[n]]:
                     graph[lista_przystankow[n]].append((lista_przystankow[1], 1))
-            elif n == len(lista_przystankow)-1:
-                if not (lista_przystankow[n-1], 1) in graph[lista_przystankow[n]]:
-                    graph[lista_przystankow[n]].append((lista_przystankow[n-1], 1))
+            elif n == len(lista_przystankow) - 1:
+                if not (lista_przystankow[n - 1], 1) in graph[lista_przystankow[n]]:
+                    graph[lista_przystankow[n]].append((lista_przystankow[n - 1], 1))
             else:
-                if not (lista_przystankow[n-1], 1) in graph[lista_przystankow[n]]:
-                    graph[lista_przystankow[n]].append((lista_przystankow[n-1], 1))
-                if not (lista_przystankow[n+1], 1) in graph[lista_przystankow[n]]:
-                    graph[lista_przystankow[n]].append((lista_przystankow[n+1], 1))
+                if not (lista_przystankow[n - 1], 1) in graph[lista_przystankow[n]]:
+                    graph[lista_przystankow[n]].append((lista_przystankow[n - 1], 1))
+                if not (lista_przystankow[n + 1], 1) in graph[lista_przystankow[n]]:
+                    graph[lista_przystankow[n]].append((lista_przystankow[n + 1], 1))
 
+    return graph
+
+
+def main():
+    with open('przystanki.json', "r", encoding='utf-8') as read_file:
+        przystanki = json.load(read_file)
+
+    with open('tramwaje.json', "r", encoding='utf-8') as read_file:
+        tramwaje = json.load(read_file)
+
+    graph = create_graph(tramwaje)
 
     for i in graph.items():
         print(i)
-
-    #for n in graph:
-        #print(n + ":" + graph[n])
-
-     #  for j in range(ilosc_linii):
-        #    tprzystanki = tramwaje["tramwaje"][j]["tprzystanki"]
-         #   for n in tprzystanki:
-            #    if n["name"] == ""
-
-         #   for n in tprzystanki:
-           #     print(n)
-
-    # print(tramwaje["tramwaje"][0]["name"])
-
-   # graph["Agatowa"].append(("plac", 1))
-    #graph["Agatowa"].append(("rondo", 1))
-
-   # print(graph["Agatowa"])
-
-
 
 
 if __name__ == '__main__':
