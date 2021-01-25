0c26bf3c08259660b94a9ea6af9b2b8cfb782caa
Denwiliusz27 <danwielgosz@gmail.com>
2021-01-25 17:09:55 +0100

    zad_1 v0.02 - nearly sorted list

--- Zestaw_9/Zadanie_1/sort_module.py
+++ Zestaw_9/Zadanie_1/sort_module.py
@@ -1,20 +1,35 @@
 import random
 
 
+def swap(lista, i, j):
+    lista[i], lista[j] = lista[j], lista[i]
+
+
 def insert_sort(lista):
+    print("lista: ", end="")
+    print(lista)
     for i in range(1, len(lista)):
         for j in range(i, 0, -1):
-            if lista[i] < lista[j]:
-                
+            if lista[j] < lista[j - 1]:
+                swap(lista, j - 1, j)
+
+    return lista
+
+
+def bubblesort(lista):
+    for i in range(0, len(lista)):
+        for j in range(0, len(lista)):
+            if lista[j] > lista[j + 1]:
+                swap(lista, j + 1, j)
 
 
 def random_list(n):
     lista = list()
 
     for i in range(0, n):
-        temp = random.randint(0, n-1)
+        temp = random.randint(0, n - 1)
         while temp in lista:
-            temp = random.randint(0, n-1)
+            temp = random.randint(0, n - 1)
 
         lista.append(temp)
 
@@ -22,6 +37,26 @@ def random_list(n):
 
 
 def nearly_sorted(n):
-    lista = random_list(10)
+    lista = insert_sort(random_list(n))
 
+    print(len(lista))
+    print(lista)
+    ile_zmian = len(lista)//3
+    print("ile zmian = ", ile_zmian)
 
+    indeksy = list()
+    for i in range(ile_zmian):
+        temp = random.randint(1, len(lista)-1)
+        while temp in indeksy:
+            temp = random.randint(1, len(lista)-1)
+        indeksy.append(temp)
+
+    print("po dodaniu")
+    print(indeksy)
+    for i in range(len(indeksy)):
+        print("i=" + str(i))
+        print("indeksy[i]=" + str(indeksy[i]))
+        swap(lista, indeksy[i], indeksy[i]-1)
+        print(lista)
+
+    return lista
--- Zestaw_9/Zadanie_1/zad_1.py
+++ Zestaw_9/Zadanie_1/zad_1.py
@@ -1,3 +1,14 @@
+"""
+ZADANIE 11.1
+Przygotować moduł Pythona z funkcjami tworzącymi listy liczb całkowitych do sortowania. Przydatne są m.in. następujące rodzaje danych:
+(a) różne liczby int od 0 do N-1 w kolejności losowej,
+(b) różne liczby int od 0 do N-1 prawie posortowane (liczby są blisko swojej prawidłowej pozycji),
+(c) różne liczby int od 0 do N-1 prawie posortowane w odwrotnej kolejności,
+(d) N liczb float w kolejności losowej o rozkładzie gaussowskim,
+(e) N liczb int w kolejności losowej, o wartościach powtarzających się, należących do zbioru k elementowego (k < N, np. k*k = N).
+"""
+
 import sort_module as s_m
 
-print(s_m.random_list(10))
+#print(s_m.insert_sort(s_m.random_list(10)))
+print(s_m.nearly_sorted(10))
