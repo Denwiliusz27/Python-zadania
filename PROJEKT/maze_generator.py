3b78e53d3840037f00fda3ba1c7e643d0448ce46
Denwiliusz27 <danwielgosz@gmail.com>
2021-02-18 00:04:36 +0100

    v2.0 - all methods added to documentation

--- PROJEKT/maze_generator.py
+++ PROJEKT/maze_generator.py
@@ -50,7 +50,8 @@ def compare_cells(cell1, cell2):
 
 def draw_grid(grid, x_amount, y_amount, cell_w):
     """
-        Służy do stworzenia i narysowania w oknie kraty, będącej podstawą do budowy labiryntu
+        Służy do stworzenia i narysowania w oknie kraty, będącej podstawą do budowy labiryntu (postawienia wszystkich
+        możliwych ścian)
 
         :param grid: słownik, którego kluczami są komórki, a wartościami listy ich sąsiadów
         :param x_amount: ilość kolumn
@@ -196,6 +197,7 @@ def draw_maze(grid, cell_w):
         :param cell_w: szerokość pojedynczej komórki
         :return: uzupełniony słownik
     """
+    time.sleep(1)
     stack = []  # stos na który odkładane są komórki, których sąsiadów należy sprawdzić
     visited = []  # lista odwiedzonych komórek
 
@@ -206,14 +208,14 @@ def draw_maze(grid, cell_w):
     pygame.display.flip()
 
     while len(stack) > 0:  # pętla wykonująca się dopóki na stosie znajdują się komórki
-        visited.append(cell) # komórka zostaje oznaczona jako odwiedzona
+        visited.append(cell)  # komórka zostaje oznaczona jako odwiedzona
 
         directions = []  # lista dostępnych kierunków ruchu, u - góra, r - prawo, d - dół, l - lewo
-        time.sleep(0.05)
+        time.sleep(0.07)
 
         if (Cell(cell.x, cell.y - cell_w).in_list(visited) is False) and (  # sprawdza czy komórka powyżej danej komórki
                 Cell(cell.x, cell.y - cell_w).in_list(list(grid.keys())) is True):  # nie została jeszcze odwiedzona i
-            directions.append("u")                                          # czy jest ona wśród kluczy słownika
+            directions.append("u")  # czy jest ona wśród kluczy słownika
         if (Cell(cell.x + cell_w, cell.y).in_list(visited) is False) and (  # sprawdza komórkę po prawej
                 Cell(cell.x + cell_w, cell.y).in_list(list(grid.keys())) is True):
             directions.append("r")
@@ -223,11 +225,12 @@ def draw_maze(grid, cell_w):
         if (Cell(cell.x - cell_w, cell.y).in_list(visited) is False) and (  # sprawdza komórkę po lewej
                 Cell(cell.x - cell_w, cell.y).in_list(list(grid.keys())) is True):
             directions.append("l")  # jeśli warunki zostały spełnione, odpowiedni kierunek zostaje dodany do listy jako
-                                    # opcja kolejnego ruchu
+            # opcja kolejnego ruchu
         if len(directions) > 0:
-            rand_direction = random.choice(directions)  # zostaje wybrany randomowy kierunek z listy dostępnych kierunków
-                                                        # kierunków
-            if rand_direction == "u": # wybrano kierunek w górę, zostaje usunięta górna krawędź komórki przez nadpisanie
+            rand_direction = random.choice(
+                directions)  # zostaje wybrany randomowy kierunek z listy dostępnych kierunków
+            # kierunków
+            if rand_direction == "u":  # wybrano kierunek w górę, zostaje usunięta górna krawędź komórki przez nadpisanie
                 draw_up(grid, cell, cell_w)  # Komórka powyżej zostaje wybrana jako nowa komórka, zostaje odpowiednio
                 cell = Cell(cell.x, cell.y - cell_w)  # zaznaczona w labiryncie. Ustalana jest relacja sasiedztwa.
             elif rand_direction == "r":
@@ -240,13 +243,13 @@ def draw_maze(grid, cell_w):
                 draw_left(grid, cell, cell_w)
                 cell = Cell(cell.x - cell_w, cell.y)
             stack.append(cell)  # wybrana komórka zostaje dodana na stos
-            time.sleep(0.05)
-            draw_creating_cell(cell)  # wybrana komórka zostaje odpowiednio zaznaczona
+            time.sleep(0.07)
+            draw_creating_cell(cell)  # wybrana komórka tworząca labirynt zostaje odpowiednio zaznaczona
             pygame.display.flip()
-        else:
-            draw_single_cell(cell)
-            time.sleep(0.05)
-            cell = stack.pop()
+        else:  # w przypadku kiedy dla danej komórki nie ma możliwości poruszenia w żadnym kierunku
+            draw_single_cell(cell)  #
+            time.sleep(0.07)
+            cell = stack.pop()  # ze stosu zostaje pobrana i usunięta ostatnio dodana komórka
             draw_creating_cell(cell)
 
     draw_single_cell(cell)
@@ -254,29 +257,35 @@ def draw_maze(grid, cell_w):
 
 
 def find_path(grid):
+    """
+        Służy do znalezienia ścieżki między komórą w lewym górnym rogu, a komórką w prawym dolnym.
+        Założenie podobne jak przy tworzeniu labiryntu.
+
+        :param grid: słownik, na którego podstawie szukana jest ścieżka
+    """
     last_nr = len(list(grid.keys()))
-    last_cell = list(grid.keys())[last_nr - 1]
+    last_cell = list(grid.keys())[last_nr - 1]  # komórka będąca w prawym dolnym rogu labiryntu (końcowa)
 
-    cell = list(grid.keys())[0]
-    stack = []
-    visited = []
+    cell = list(grid.keys())[0]  # komórka w lewym górnym rogu (startowa)
+    stack = []  # stos na który odkładane są komórki, których sąsiadów należy sprawdzić
+    visited = []  # lista odwiedzonych komórek
 
-    while not compare_cells(cell, last_cell):
-        neighbours = []
-        visited.append(cell)
-        draw_path_cell(cell)
+    while not compare_cells(cell, last_cell):  # pętla wykonująca się, dopóki program nie natrafi na komórkę końcową
+        neighbours = []  # lista sąsiadów danej komórki
+        visited.append(cell)  # komórka zostaje dodana do listy komórek odwiedzonych
+        draw_path_cell(cell)  # w podanej komórce zostaje narysowany punkt będący elementem ścieżki
 
-        for i in grid.keys():
-            if compare_cells(cell, i):
+        for i in grid.keys():  # pętla dodaje do listy sąsiadów komórki, do których można przejść z podanej komórki (nie
+            if compare_cells(cell, i):  # wystepuje między nimi ściana), a którzy nie zostali jeszcze odwiedzeni
                 for j in grid[i]:
                     if not j.in_list(visited):
                         neighbours.append(j)
                 break
 
-        if len(neighbours) > 0:
-            stack.append(cell)
+        if len(neighbours) > 0:  # jeśli komórka posiada nieodwiedzonych sasiadów, zostaje wybrany jeden z nich, a
+            stack.append(cell)   # komórka dodana na stos
             cell = random.choice(neighbours)
-        else:
+        else:  # jeśli wszyscy sąsiedzi komórki zostali odwiedzeni, zostaje pobrana i usunięta komórka ze szczytu stosu
             time.sleep(0.15)
             draw_single_cell(cell)
             cell = stack.pop()
@@ -286,13 +295,16 @@ def find_path(grid):
     draw_path_cell(cell)
 
 
-def main():
+def main_loop():
+    """
+        Główna pętla programu
+    """
     working = True
     while working:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 working = False
-        clock.tick(30)
+        clock.tick(60)
 
 
 if __name__ == '__main__':
@@ -301,21 +313,21 @@ if __name__ == '__main__':
     WHITE = (255, 255, 255)
     BLUE = (0, 0, 255)
 
-    x_cells = 15
-    y_cells = 15
-    cell_w = 25
+    x_cells = 15  # ilość komórek w poziomie
+    y_cells = 15  # ilość komórek w pionie
+    cell_w = 25  # szerokość komórki
 
     pygame.init()
-    width = 50 + x_cells * cell_w
-    hight = 50 + y_cells * cell_w
+    width = 50 + x_cells * cell_w  # szerokość okna dostosowana do wielkości labiryntu
+    hight = 50 + y_cells * cell_w  # wysokość okna dostosowana do wielkości labiryntu
 
     screen = pygame.display.set_mode((width, hight))
     pygame.display.set_caption("Maze generator")
     clock = pygame.time.Clock()
 
-    grid = {}
+    grid = {}  # słownik wykorzystywany w reszcie funkcji
 
     grid = draw_grid(grid, x_cells, y_cells, cell_w)
     grid = draw_maze(grid, cell_w)
     find_path(grid)
-    main()
+    main_loop()
